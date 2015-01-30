67f047e72355d77ffb633861527eabae37036640
ddrccw <ddrccw@gmail.com>
2015-01-30 18:57:16 +0800

    MOD: 暂时取消alert background style的参数控制

--- SIAlertView/SIAlertView.m
+++ SIAlertView/SIAlertView.m
@@ -97,30 +97,30 @@ static SIAlertView *__si_alert_current_view;
 
 - (void)drawRect:(CGRect)rect
 {
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    switch (self.style) {
-        case SIAlertViewBackgroundStyleGradient:
-        {
-            size_t locationsCount = 2;
-            CGFloat locations[2] = {0.0f, 1.0f};
-            CGFloat colors[8] = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.75f};
-            CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-            CGGradientRef gradient = CGGradientCreateWithColorComponents(colorSpace, colors, locations, locationsCount);
-            CGColorSpaceRelease(colorSpace);
-            
-            CGPoint center = CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2);
-            CGFloat radius = MIN(self.bounds.size.width, self.bounds.size.height) ;
-            CGContextDrawRadialGradient (context, gradient, center, 0, center, radius, kCGGradientDrawsAfterEndLocation);
-            CGGradientRelease(gradient);
-            break;
-        }
-        case SIAlertViewBackgroundStyleSolid:
-        {
-            [[UIColor colorWithWhite:0 alpha:0.5] set];
-            CGContextFillRect(context, self.bounds);
-            break;
-        }
-    }
+//    CGContextRef context = UIGraphicsGetCurrentContext();
+//    switch (self.style) {
+//        case SIAlertViewBackgroundStyleGradient:
+//        {
+//            size_t locationsCount = 2;
+//            CGFloat locations[2] = {0.0f, 1.0f};
+//            CGFloat colors[8] = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.75f};
+//            CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+//            CGGradientRef gradient = CGGradientCreateWithColorComponents(colorSpace, colors, locations, locationsCount);
+//            CGColorSpaceRelease(colorSpace);
+//            
+//            CGPoint center = CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2);
+//            CGFloat radius = MIN(self.bounds.size.width, self.bounds.size.height) ;
+//            CGContextDrawRadialGradient (context, gradient, center, 0, center, radius, kCGGradientDrawsAfterEndLocation);
+//            CGGradientRelease(gradient);
+//            break;
+//        }
+//        case SIAlertViewBackgroundStyleSolid:
+//        {
+//            [[UIColor colorWithWhite:0 alpha:0.5] set];
+//            CGContextFillRect(context, self.bounds);
+//            break;
+//        }
+//    }
 }
 
 @end
